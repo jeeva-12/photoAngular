f90631be1ae37b5212397f4bc9a96cc9e3092c98
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-29 15:19:27 +0530

    hgtf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
+import {HttpClientModule} from '@angular/common/http';
+
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:ViewComponent
+
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(myRoute),
+    HttpClientModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
